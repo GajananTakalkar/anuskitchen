9717b0cb9e8606055f2f65ed5db4e8a34d52f4b0
GajananTakalkar <113528562+GajananTakalkar@users.noreply.github.com>
2025-11-16 20:29:54 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/product')
+def product():
+    return render_template('product.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
